ed038a89c7f6034b5a0ae371a4560985199d156e
han yu <1807003141@qq.com>
2018-07-01 01:25:10 +0800

    完成导航布局样式.

--- src/components/nav_table/nav_table.vue
+++ src/components/nav_table/nav_table.vue
@@ -1,20 +1,11 @@
 <template>
   <div id="nav_table">
-    <router-link tag="div" class="nav-tab" :to={ name: "Recommend" }>
-      <span>{{ nav.recommend }}</span>
-    </rotuer-link>
-
-    <router-link tag="div" class="nav-tab" :to={ name: "Singer" }>
-      <span>{{ nav.singer }}</span>
-    </rotuer-link>
-
-    <router-link tag="div" class="nav-tab" :to={ name: "RankList" }>
-      <span>{{ nav.rankList }}</span>
-    </rotuer-link>
-
-    <router-link tag="div" class="nav-tab" :to={ name: "Search" }>
-      <span>{{ nav.search }}</span>
-    </rotuer-link>
+    <!-- 顶部导航页面 -->
+    <router-link tag="div" v-for="(item, key, index) in nav" :key="index" class="nav-tab" :to="{ name: key }">
+      <span class="nav-title" @click="moveBottomLine(index)">{{ item }}</span>
+    </router-link>
+    <!-- 下划线 -->
+    <article class="bottom-line" ref="bottomLine"></article>
   </div>
 </template>
 
@@ -29,6 +20,17 @@ export default {
         search: '搜索'
       }
     };
+  },
+  methods: {
+    /*
+      位移下划线
+    */
+    moveBottomLine(index) {
+      let navCount = Object.keys(this.nav).length;
+      let divide = 100 / navCount; // 一等分长度
+      let moveLen = index * divide; // 位移长度
+      this.$refs.bottomLine.style.left = `${ moveLen }%`;
+    }
   }
 };
 </script>
