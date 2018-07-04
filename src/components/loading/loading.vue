e39b4f027f589e5f4b7f56e6d423b45ed2c7f7e3
han yu <1807003141@qq.com>
2018-07-05 01:31:34 +0800

    feat: 使用 vue-lazyload 懒加载 & 新增 loading.vue 组件.

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="loading">
+    <img width="24" height="24" src="./loading.gif" />
+    <p class="desc">{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '正在加载...'
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/const.scss';
+
+#loading {
+  width: 100%;
+  text-align: center;
+  .desc {
+    line-height: 20px;
+    font-size: $font-size-small;
+    color: $color-theme;
+  }
+}
+</style>
