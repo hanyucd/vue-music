fa859750b921a3578b93c6f2e549086ba4eb7e2d
han yu <1807003141@qq.com>
2018-07-22 19:36:43 +0800

    feat: 实现歌曲列表下拉图片放大效果.

--- src/page/singer_detail/music_list/music_list.vue
+++ src/page/singer_detail/music_list/music_list.vue
@@ -8,10 +8,10 @@
     <h1 class="title" v-html="title"></h1>
     <!-- 头部背景图片 -->
     <section class="bg-image" :style="bgStyle" ref="bgImage">
-      <!-- 背景暗灰蒙层 -->
+      <!-- 背景图片上 暗灰蒙层 -->
       <div class="filter"></div>
     </section>
-    <!-- 推层 -->
+    <!-- 歌曲列表上滑 推层上移效果 -->
     <section class="bg-layer" ref="bgLayer"></section>
 
     <scroll
@@ -60,7 +60,7 @@ export default {
   data() {
     return {
       scrollY: 0
-    }
+    };
   },
   computed: {
     bgStyle() {
@@ -71,7 +71,30 @@ export default {
     scrollY(newY) {
       // 固定推层 Y 轴上限为背景图片可视高度
       let translateY = Math.max(this.minTranslateY, newY);
+      let z_index = 0; // 声明背景图片层叠优先级
+      let scale = 1; // 声明背景图片放大比例
+      // 背景图 Dom
+      let bgImageDom = this.$refs.bgImage;
       this.$refs.bgLayer.style['transform'] = `translate3d(0, ${ translateY }px, 0)`;
+
+      // 计算下拉图片放大比例
+      const percent = Math.abs(newY / this.imageHeight);
+      if (newY > 0) {
+        scale = 1 + percent;
+        z_index = 10;
+      }
+      bgImageDom.style['transform'] = `scale(${ scale })`;
+
+      // 如果歌曲列表上滑Y轴 小于 限制高度
+      if (newY < this.minTranslateY) {
+        z_index = 10;
+        bgImageDom.style.paddingTop = 0;
+        bgImageDom.style.height = `${ RESERVED_HEIGHT }px`;
+      } else {
+        bgImageDom.style.paddingTop = '70%';
+        bgImageDom.style.height = '0';
+      }
+      bgImageDom.style.zIndex = z_index;
     }
   },
   created() {
