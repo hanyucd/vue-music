20236dd30482d7f7ecd43c5ad48c2ae165eb54c4
han yu <1807003141@qq.com>
2018-08-02 17:57:02 +0800

    feat: 新增播放器页面动画效果.

--- src/page/player/player.vue
+++ src/page/player/player.vue
@@ -1,66 +1,71 @@
 <template>
   <div id="player" v-show="playlist.length > 0">
     <!-- 最大化的播放器 -->
-    <section class="normal-player" v-show="fullScreen">
-      <!-- 背景图 -->
-      <div class="background">
-        <img :src="currentSong.image" width="100%" height="100%"/>
-      </div>
-       <!-- 播放器顶部 -->
-      <div class="top">
-        <!-- 返回按钮 -->
-        <div class="back" @click="miniPlayer">
-          <i class="icon-back"></i>
+    <transition name="normal">
+      <section class="normal-player" v-show="fullScreen">
+        <!-- 背景图 -->
+        <div class="background">
+          <img :src="currentSong.image" width="100%" height="100%"/>
         </div>
-        <h1 class="title" v-html="currentSong.name"></h1>
-        <h2 class="subtitle" v-html="currentSong.singer"></h2>
-      </div>
-      <!-- 播放器中间 -->
-      <div class="middle">
-        <!-- 唱片 -->
-        <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd">
-              <img :src="currentSong.image" class="image"/>
+        <!-- 播放器顶部 -->
+        <div class="top">
+          <!-- 返回按钮 -->
+          <div class="back" @click="miniPlayer">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        </div>
+        <!-- 播放器中间 -->
+        <div class="middle">
+          <!-- 唱片 -->
+          <div class="middle-l">
+            <div class="cd-wrapper">
+              <div class="cd">
+                <img :src="currentSong.image" class="image"/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-       <!-- 播放器底部 -->
-      <div class="bottom">
-        <div class="operators">
-          <div class="icon i-left">
-            <i class="icon-sequence"></i>
-          </div>
-          <div class="icon i-left">
-            <i class="icon-prev"></i>
-          </div>
-          <div class="icon i-center">
-            <i class="icon-play"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon icon-next"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon icon-not-favorite"></i>
+        <!-- 播放器底部 -->
+        <div class="bottom">
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon i-left">
+              <i class="icon-prev"></i>
+            </div>
+            <div class="icon i-center">
+              <i class="icon-play"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon icon-not-favorite"></i>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-     <!-- 最小化的播放器 -->
-    <section class="mini-player" v-show="!fullScreen" @click="maxPlayer">
-      <div class="icon">
-        <img :src="currentSong.image" width="40" height="40" />
-      </div>
-      <div class="text">
-        <h2 class="name" v-html="currentSong.name"></h2>
-        <p class="desc" v-html="currentSong.singer"></p>
-      </div>
-      <div class="control"></div>
-      <div class="control">
-        <i class="icon-playlist"></i>
-      </div>
-    </section>
+      </section>
+    </transition>
+
+    <!-- 最小化的播放器 -->
+    <transition name="mini">
+      <section class="mini-player" v-show="!fullScreen" @click="maxPlayer">
+        <div class="icon">
+          <img :src="currentSong.image" width="40" height="40" />
+        </div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control"></div>
+        <div class="control">
+          <i class="icon-playlist"></i>
+        </div>
+      </section>
+    </transition>
   </div>
 </template>
 
