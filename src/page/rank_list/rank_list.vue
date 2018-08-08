f1fc81e1c211b86f150ef6e7deeb4af9cddbe8df
han yu <1807003141@qq.com>
2018-08-08 17:00:32 +0800

    feat: 完成排行榜页面逻辑.

--- src/page/rank_list/rank_list.vue
+++ src/page/rank_list/rank_list.vue
@@ -1,22 +1,28 @@
 <template>
-  <div id="rank_list">
-    <div class="top-list">
+  <div id="rank_list" ref="rank">
+    <scroll class="top-list" :data="rankList" ref="rank_list">
       <ul>
-        <li class="item">
+        <li class="item" v-for="item of rankList" :key="item.id">
            <!-- 左边图片 -->
           <div class="icon">
-            <img width="100" height="100" />
+            <img width="100" height="100" v-lazy="item.picUrl"/>
           </div>
           <!-- 右边歌曲 -->
           <ul class="song-list">
-            <li class="song">
-              <span></span>
-              <span></span>
+            <li class="song" v-for="(song, index) of item.songList" :key="index">
+              <span>{{ index + 1 }}.</span>
+              <span>{{ song.songname }}</span>
+              <span> - {{ song.singername }}</span>
             </li>
           </ul>
         </li>
       </ul>
-    </div>
+
+      <!-- loading 组件 -->
+      <article class="loading-container" v-show="!rankList.length">
+        <Loading></Loading>
+      </article>
+    </scroll>
 
     <router-view></router-view>
   </div>
@@ -26,7 +32,19 @@
 import { getRankList } from '@/api/rank';
 import { ERROR_OK } from '@/api/config';
 
+import Scroll from '@/components/scroll/scroll';
+import Loading from '@/components/loading/loading';
+
+import { playlistMixin } from '@/assets/js/mixin';
+
 export default {
+  mixins: [
+    playlistMixin
+  ],
+  components: {
+    Scroll,
+    Loading
+  },
   data() {
     return {
       rankList: []
@@ -39,9 +57,17 @@ export default {
     _fetchRankList() {
       getRankList().then(res => {
         if (res.code === ERROR_OK) {
-          console.log(res.data.topList);
+          this.rankList = res.data.topList;
         }
       });
+    },
+    /*
+     * 当有迷你播放器时，调整滚动底部距离
+     */
+    handlePlaylist(playlist) {
+      let bottom = playlist.length > 0 ? '60px' : '';
+      this.$refs.rank.style.bottom = bottom;
+      this.$refs.rank_list.refresh();
     }
   }
 };
