062beb316fd827b760389c3e27c8863e9415b58a
han yu <1807003141@qq.com>
2018-08-08 12:37:32 +0800

    feat: 完成歌单详情数据逻辑.

--- src/page/recommend_songList/recommend_songList.vue
+++ src/page/recommend_songList/recommend_songList.vue
@@ -1,18 +1,24 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bgImage="bgImage"></music-list>
+    <music-list :title="title" :bgImage="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script>
 import MusicList from '@/components/music_list/music_list';
 import { getSongList } from '@/api/recommend';
+import { createSong } from '@/assets/js/song';
 
 import { mapGetters } from 'vuex';
 
 export default {
   components: {
     MusicList
+  },
+  data() {
+    return {
+      songs: []
+    };
   },
   computed: {
     ...mapGetters([
@@ -33,12 +39,33 @@ export default {
   methods: {
     // 获取歌单数据
     _getSongList() {
+      if (!this.songList.dissid) {
+        this.$router.push('/recommend');
+        return;
+      }
+
       getSongList(this.songList.dissid)
         .then(res => {
           if (res.code === 0) {
-            console.log('hanyu:', res.cdlist[0].songlist);
+            this.songs = this._formatSongs(res.cdlist[0].songlist);
           }
         });
+    },
+    /*
+     * 重组 res.cdlist[0].songlist 数据
+     */
+    _formatSongs(list) {
+      let result = [];
+
+      list.forEach(item => {
+        if (item.songid && item.albummid) {
+          createSong(item).then(res => {
+            result.push(res);
+          });
+        }
+      });
+
+      return result;
     }
   }
 };
