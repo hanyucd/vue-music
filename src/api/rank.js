986e15e45ec08d25f0d27cd9f1f94dcc44a7cd00
han yu <1807003141@qq.com>
2018-08-08 18:27:58 +0800

    feat: 处理排行榜详情页数据逻辑.

--- src/api/rank.js
+++ src/api/rank.js
@@ -2,7 +2,7 @@ import jsonp from '@/assets/js/jsonp';
 import { commonParams, options } from './config';
 
 /**
- * jsonp 抓取排行页数据
+ * jsonp 抓取排行榜页面数据
  *
  * 接口（移动端）：https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg
  */
@@ -16,4 +16,25 @@ export function getRankList() {
   });
 
   return jsonp(url, data, options);
-}
+};
+
+/**
+ * jsonp 抓取排行榜详情页数据
+ *
+ * 接口：https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg
+ */
+export function getRankDetail(topid) {
+  let url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg';
+
+  let data = Object.assign({}, commonParams, {
+    g_tk: 5381,
+    needNewCode: 1,
+    platform: 'h5',
+    page: 'detail',
+    type: 'top',
+    tpl: 3,
+    topid
+  });
+
+  return jsonp(url, data, options);
+};
--- src/page/rank_detail/rank_detail.vue
+++ src/page/rank_detail/rank_detail.vue
@@ -1,17 +1,26 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bgImage="bgImage"></music-list>
+    <music-list :title="title" :bgImage="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script>
 import MusicList from '@/components/music_list/music_list';
 
+import { getRankDetail } from '@/api/rank';
+
+import { createSong } from '@/assets/js/song';
+
 import { mapGetters } from 'vuex';
 
 export default {
   components: {
     MusicList
+  },
+  data() {
+    return {
+      songs: []
+    };
   },
   computed: {
     ...mapGetters([
@@ -21,7 +30,48 @@ export default {
       return this.rankList.topTitle;
     },
     bgImage() {
-      return this.rankList.picUrl;
+      if (this.songs.length) {
+        // 返回第一首歌曲图片
+        return this.songs[0].image;
+      }
+      return '';
+    }
+  },
+  created() {
+    this._fetchRankDetail();
+  },
+  methods: {
+    /*
+     * 获取指定排行榜单详情
+     */
+    _fetchRankDetail() {
+      // 禁止直接刷新详情页（获取不到排行 id）
+      if (!this.rankList.id) {
+        this.$router.push('/rank');
+        return;
+      }
+
+      getRankDetail(this.rankList.id).then(res => {
+        if (res.code === 0) {
+          this.songs = this._formatSongs(res.songlist);
+        }
+      });
+    },
+    /*
+     * 重组 res.songlist 数据
+     */
+    _formatSongs(list) {
+      let result = [];
+
+      list.forEach(item => {
+        let musicData = item.data;
+        if (musicData.songid && musicData.albummid) {
+          createSong(musicData).then(data => {
+            result.push(data);
+          });
+        }
+      });
+      return result;
     }
   }
 };
