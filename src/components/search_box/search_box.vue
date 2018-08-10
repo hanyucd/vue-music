5e138cfdf787caca2333739508be2b35933dc15a
han yu <1807003141@qq.com>
2018-08-10 20:12:12 +0800

    feat: 编写搜索组件结构 & 样式.

---
+++ src/components/search_box/search_box.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="search_box">
+    <i class="icon-serch"></i>
+    <input class="box" type="text"/>
+    <i class="icon-dismiss"></i>
+  </div>
+</template>
+
+<script>
+default export {
+
+};
+</script>
+
+<style lang="scss" scoped>
+  @import './search_box.scss';
+</style>
