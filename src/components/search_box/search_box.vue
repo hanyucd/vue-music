af2d2ef58adda6541d65dcae0129c69266f36e64
han yu <1807003141@qq.com>
2018-08-10 20:47:32 +0800

    feat: search_box 组件添加逻辑处理.

--- src/components/search_box/search_box.vue
+++ src/components/search_box/search_box.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="search_box">
-    <i class="icon-serch"></i>
-    <input class="box" type="text"/>
-    <i class="icon-dismiss"></i>
+    <i class="icon-search"></i>
+    <input class="box" type="text" v-model="query" :placeholder="placeholder" />
+    <i class="icon-dismiss" v-show="query" @click="clear"></i>
   </div>
 </template>
 
 <script>
-default export {
-
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: '搜索歌曲、歌手'
+    }
+  },
+  data() {
+    return {
+      query: ''
+    };
+  },
+  created() {
+    this.$watch('query', (newQuery) => {
+      this.$emit('query', newQuery);
+    });
+  },
+  methods: {
+    /*
+     * 清空输入框
+     */
+    clear() {
+      this.query = '';
+    }
+  }
 };
 </script>
 
--- src/page/search/search.vue
+++ src/page/search/search.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    {{ msg }}
+  <div id="search">
+    <!-- 搜索框 -->
+    <article class="search-box-wrapper">
+      <search-box></search-box>
+    </article>
   </div>
 </template>
 
 <script>
+import SearchBox from '@/components/search_box/search_box';
+
 export default {
+  components: {
+    SearchBox
+  },
   data() {
     return {
       msg: '搜索'
