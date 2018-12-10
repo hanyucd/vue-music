33bcdedef0b27d5ba8f0dc9f74e4c71179f97f4a
han yu <1807003141@qq.com>
2018-12-10 21:10:08 +0800

    feat: 创建播放列表组件.

---
+++ src/page/play_list/play_list.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export function {
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  @import './play_list.scss';
+</style>
