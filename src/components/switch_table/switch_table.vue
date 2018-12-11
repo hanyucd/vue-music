21083b00df840355d23364799a3070aabc8a653f
han yu <1807003141@qq.com>
2018-12-11 18:10:48 +0800

    feat: 完成切换开关组件.

---
+++ src/components/switch_table/switch_table.vue
@@ -0,0 +1,60 @@
+<template>
+  <ul id="switch">
+    <li class="switch-item"
+      v-for="(item, index) of switches"
+      :key="index"
+      :class="{ active: currentIndex === index }"
+      @click="switchItem(index)"
+    >
+      <span>{{ item.name }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    switches: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    currentIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    // 开关切换，通知父组件
+    switchItem(index) {
+      this.$emit('switch', index);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+  @import '@/assets/scss/const.scss';
+
+  #switch {
+    display: flex;
+    align-items: center;
+    width: 250px;
+    margin: 0 auto;
+    border: 1px solid $color-highlight-background;
+    border-radius: 60px;
+    overflow: hidden;
+    .switch-item {
+      flex: 1;
+      padding: 8px;
+      text-align: center;
+      font-size: $font-size-medium;
+      color: $color-text-d;
+      &.active {
+        background: $color-highlight-background;
+        color: $color-text;
+      }
+    }
+  }
+</style>
--- src/page/play_list/add_song/add_song.vue
+++ src/page/play_list/add_song/add_song.vue
@@ -11,32 +11,71 @@
 
        <!-- 搜索框 -->
       <section class="search-box-wrapper">
+        <search-box ref="searchBox" placeholder="搜索歌曲" v-on:query="onQueryChange"></search-box>
       </section>
 
       <!-- 切换区块 -->
-      <section class="shortcut">
+      <section class="shortcut" v-show="!query">
+        <switch-table :switches="switches" :currentIndex="currentIndex" v-on:switch="switchTable"></switch-table>
       </section>
 
       <!-- 搜索区块 -->
-      <section class="search-result">
+      <section class="search-result" v-show="query">
+        <suggest :query="query" :zhida="zhida" v-on:select="selectSuggest"></suggest>
       </section>
     </div>
   </transition>
 </template>
 
 <script>
+import SearchBox from '@/components/search_box/search_box';
+import Suggest from '@/components/suggest/suggest';
+import SwitchTable from '@/components/switch_table/switch_table';
+
+import { mapActions } from 'vuex';
+
 export default {
+  components: {
+    SearchBox,
+    Suggest,
+    SwitchTable
+  },
   data() {
     return {
-      showFlag: false
+      showFlag: false,
+      query: '',
+      zhida: false, // 不搜索歌手
+      switches: [
+        { name: '最近播放' },
+        { name: '搜索历史' }
+      ],
+      currentIndex: 0 // 当前切换开关
     };
   },
   methods: {
+    ...mapActions([
+      'saveSearchHistory'
+    ]),
     show() {
       this.showFlag = true;
     },
     hide() {
       this.showFlag = false;
+    },
+    onQueryChange(query) {
+      this.query = query;
+    },
+    /*
+     * 切换 Table
+     */
+    switchTable(index) {
+      this.currentIndex = index;
+    },
+    /*
+     * 缓存搜索历史 | 调 Actions
+     */
+    selectSuggest() {
+      this.saveSearchHistory(this.query);
     }
   }
 };
